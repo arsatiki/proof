7787b5648743176d98cc731f23b029498b87b0f7
Antti Rasinen <antti.rasinen@codento.com>
2015-03-23 23:30:57 +0200

    Make Reader32 pass CRC32 test

--- reader.go
+++ reader.go
@@ -1,9 +1,12 @@
 package proof
 
 import (
+	"errors"
 	"hash"
 	"io"
 )
+
+var EHASHFAIL error = errors.New("checksum mismatch")
 
 type Reader32 struct {
 	r   io.Reader
@@ -11,10 +14,21 @@ type Reader32 struct {
 	sum uint32
 }
 
-func NewReader32(r io.reader, h hash.Hash32, sum uint32) {
-	// WIP
+func NewReader32(r io.Reader, h hash.Hash32, sum uint32) *Reader32 {
+	if r == nil || h == nil {
+		return nil
+	}
+	r32 := &Reader32{r, h, sum}
+	return r32
 }
 
 func (r *Reader32) Read(p []byte) (int, error) {
-	// WIP
+	n, err := r.r.Read(p)
+	r.h.Write(p[:n])
+
+	if err == io.EOF && r.h.Sum32() != r.sum {
+		err = EHASHFAIL
+	}
+
+	return n, err
 }
