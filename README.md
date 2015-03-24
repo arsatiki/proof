54e26dd3a7eb514bf536616774e024440a969794
Antti Rasinen <ars@iki.fi>
2015-03-24 11:24:50 +0200

    Add notes about TeeReader

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # proof
 A proof.Reader will validate data while Read()ing it
+
+However, using `io.TeeReader` gives you much of the same terseness with less code.
+This package may not have a useful usecase.
--- reader_test.go
+++ reader_test.go
@@ -3,6 +3,7 @@ package proof
 import (
 	"bytes"
 	"hash/crc32"
+	"io"
 	"io/ioutil"
 	"testing"
 )
@@ -26,5 +27,19 @@ func TestCRC(t *testing.T) {
 		if err != c.err {
 			t.Fatalf("expected error: %v, got: %v", c.err, err)
 		}
+	}
+}
+
+func TestTeeReader(t *testing.T) {
+	data := []byte("Hello, World!")
+	sum := uint32(0xec4ac3d0)
+
+	buf := bytes.NewBuffer(data)
+	h := crc32.NewIEEE()
+	r := io.TeeReader(buf, h)
+
+	_, err := ioutil.ReadAll(r)
+	if err != nil || h.Sum32() != sum {
+		t.Fatalf("Either error %v or %d != %d", err, h.Sum32(), sum)
 	}
 }
